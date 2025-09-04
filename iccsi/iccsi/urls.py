98df84a734d92d51d63ac5a218a2448a073bf535
@cursosdc3 <danielaguila2023@gmail.com>
2025-09-04 11:06:56 -0600

    Corregir importación de views - Solucionar error 404

--- iccsi/iccsi/urls.py
+++ iccsi/iccsi/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from iccsi.views import home, api_info
+from iccsi.iccsi.views import home, api_info
 
 urlpatterns = [
        path('', home, name='home'),
