c53e1e21d45b9f0f53f1d232eba566d888044237
@cursosdc3 <danielaguila2023@gmail.com>
2025-09-04 11:20:34 -0600

    Corregir importación relativa de views - Servidor funcionando

--- iccsi/iccsi/urls.py
+++ iccsi/iccsi/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from iccsi.iccsi.views import home, api_info
+from .views import home, api_info
 
 urlpatterns = [
        path('', home, name='home'),
